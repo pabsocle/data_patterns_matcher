709f6fd501076045193e7f5e19d7e35186e8a7a3
pabluntu <pabsocle1989@gmail.com>
2023-04-07 12:10:43 +0200

    virtual functions expected to use in main

---
+++ code/BusinessLogic/IModule/inc/IModule.h
@@ -0,0 +1,12 @@
+#ifndef IMODULE_H
+#define IMODULE_H
+
+class IModule {
+    public:
+        virtual void start() = 0;
+        virtual void stop() = 0;
+        virtual void setDataReceiver(IModule *dataReceiver) = 0;
+
+}
+
+#endif // IMODULE_H
