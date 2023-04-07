ddda5e8858c4f1c9afa89ac81fc3570059da4ee4
pabluntu <pabsocle1989@gmail.com>
2023-04-07 10:57:35 +0200

    main compiling and working and IModule prepared

---
+++ code/BusinessLogic/IModule/src/IModule.cpp
@@ -0,0 +1,1 @@
+#include "IModule.h"
---
+++ code/BusinessLogic/main.cpp
@@ -0,0 +1,30 @@
+#include "code/BusinessLogic/IModule/inc/IModule.h"
+#include <iostream>
+
+int main(void) {
+    std::cout << "Start of programm\n";
+    /*
+    //Create modules
+    IModule *m1 = (get object of type module 1).
+    IModule *m2 = (get object of type module 2).
+    IModule *m3 = (get object of type module 3).
+
+    //Set m1 to deliver data to m2
+
+    //Set m2 to deliver data to m3
+
+    //Start modules
+
+    //Wait 100 seconds
+
+    //Stop modules
+
+    //Release modules
+
+    delete m1;
+    delete m2;
+    delete m3;
+    */
+    std::cout << "End of programm\n";
+    return 0;
+}
